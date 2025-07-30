d1c8c75bfbcac5104e38411130de748e7127aff7
samuelkime267 <samuelkime7@gmail.com>
2025-07-30 11:13:15 +0100

    finished express section

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const http = require("http");
-const routes = require("./routes");
+// const http = require("http");
 
-http.createServer(routes).listen(8080);
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const adminRoute = require("./routes/admin");
+const publicRoute = require("./routes/shop");
+const rootDir = require("./utils/path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use("/admin", adminRoute);
+app.use(publicRoute);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
+
+app.listen(8080);
